fa15987b1d448af5a87830cc629a38979c93a18f
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2023-06-03 23:09:02 +0530

    fixed an error

--- front end/fapp/src/App.jsx
+++ front end/fapp/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-
-
-
+import Searchcard from "./components/Searchcard";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Searchcard/>
+      <Footer/>
     </div>
   );
 }
---
+++ front end/fapp/src/components/Searchcard.jsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./Searchcard.css";
+
+function Card2() {
+  return (
+    <div className="rechargephoto">
+      <Card className="rechargephoto-card" /*onClick={handleClick}*/>
+        <Card.Img
+          variant="top"
+          src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
+        />
+      </Card>
+
+      <Card className="rechargephoto-card" /*onClick={handleClick}*/>
+        <Card.Img
+          variant="top"
+          src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
+        />
+      </Card>
+    </div>
+  );
+}
+
+export default Card2;
