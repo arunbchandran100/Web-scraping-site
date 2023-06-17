8d01ea245b5fb4f7b6ddd4590e1cd482c36feb38
kiranthomas77 <97358437+kiranthomas77@users.noreply.github.com>
2023-06-17 12:21:27 +0530

    installed rest framework

--- Back_end/web_scrapping/subapp/models.py
+++ Back_end/web_scrapping/subapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class real_estate(models.Model):
+    title=models.Charfield(max_length=150)
+    price=models.Charfield(max_length=30)
+    location=models.Charfield(max_length=150)
+    square_foot=models.Charfield(max_length=20)
+    def __str__(self):
+        return self.user #user?
