e3ffe8c367963f441fac7b5aae583a7e1d2f103f
Pompa Ghosh <97214748+floraaheh@users.noreply.github.com>
2023-06-18 17:16:55 +0530

    Update product.jsx

--- front_end/fapp/src/components/product.jsx
+++ front_end/fapp/src/components/product.jsx
@@ -1,35 +1,4 @@
-// export function Product(props) {
-//     return (
 
-//         <div className='productList'>
-//             <div key={props.id} className='productCard'>
-//                 <img src={props.image} alt='product-img' className='productImage'></img>
-
-                
-
-//                 <div className='productCard__content'>
-//   <img src="https://images.nobroker.in/images/8a9fd8828760f801018760ffd9f8018c/8a9fd8828760f801018760ffd9f8018c_60330_508297_thumbnail.jpg" alt="Product Image" />
-//   <h3 className='productName'>2 BHK House  For Sale  In Oragadam Industrial Corridor</h3>
-//   <div className='displayStack__1'>
-//     <div className='productPrice'>42 Lacs</div>
-//     <div className='productSales'>7479</div>
-//   </div>
-//   <div className='displayStack__2'>
-//     {/* <div className='productRating'>
-//         {[...Array(5)].map((_, index) => (
-//             <FaStar id={index + 1} key={index} />
-//         ))}
-//     </div> */}
-//     <div className='productTime'>1000 sq feet</div>
-//   </div>
-// </div>
-
-
-//             </div>
-//         </div>
-//     )
-// }
-// export default Product;
 import React from 'react';
 import Image from 'next/image';
 import millify from 'millify';
@@ -43,28 +12,28 @@ import Link from 'next/link';
 import "../styles/globals.css";
 
 
-const Property = ({ item }) => {
+const Product = ({ item }) => {
     const { rooms, bath, isVerified, agency: { logo }, price, title, rentFrequency, area, externalID } = item;
-//  let API = "";
-//  const  fetchApiData = async(url) =>{
-//     try{
-//         const res = await fetch(url);
-//         const data = await res.json();
-//         console.log(data);
-//     }
-//     catch(error)
-//     {
-//         console.log(error);
-//     }
-//     };
-//     useEffect(()=>
-//     {
-//         fetchApiData(API);
+ let API = "";
+ const  fetchApiData = async(url) =>{
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+    };
+    useEffect(()=>
+    {
+        fetchApiData(API);
     
-//     },[]);
+    },[]);
  
     return (
-        <Link href={`/property/${externalID}`} passHref>
+        <Link href={`/product/${externalID}`} passHref>
             <Flex flexDirection="column" cursor="pointer">
                 <Box>
                     <Image src={item.coverPhoto ? item.coverPhoto.url : item.coverPhoto} alt="bannerImage" width="450px" height="250px" />
@@ -91,4 +60,4 @@ const Property = ({ item }) => {
     );
 };
 
-export default Property;
+export default Product;
