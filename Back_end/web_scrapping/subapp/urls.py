b85da58024db8818bfa6475500a6da7cade2b878
kiranthomas77 <97358437+kiranthomas77@users.noreply.github.com>
2023-06-28 13:52:38 +0530

    3 to 1 function call

--- Back_end/web_scrapping/subapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import SubmitFormAPIView
-urlpatterns = [
-
-    path('form/', SubmitFormAPIView.as_view(), name='submit_form'),
-]
--- README.md
+++
@@ -1,2 +0,0 @@
-# Web-scraping-site
-Web scraping from property sites.
