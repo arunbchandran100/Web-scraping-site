fe15ee77b673c82f978cf7f77b66b5da2750f86a
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2023-06-29 22:54:58 +0530

    Final Working Code

--- Back_end/web_scrapping/web_scrapping/settings.py
+++ Back_end/web_scrapping/web_scrapping/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'real_estate',
 		'USER': 'root',
-		'PASSWORD': 'kishan@20',
+		'PASSWORD': 'root',
 		'HOST':'localhost',
 		'PORT':'3306',
 	}
